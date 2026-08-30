b1cf0e32414a7996ea1cc1bb117a28b3d58f2ceb
Bohdan Bats <batsbohdan@gmail.com>
2026-08-30 23:12:38 +0300

    Initialize Liquid Glass before the app starts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LiquidGlassWidgets.initialize();
   runApp(const PrinciplesApp());
 }
